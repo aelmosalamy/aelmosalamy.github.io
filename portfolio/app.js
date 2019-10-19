32818a6022930dc6605fd40a5676b594cdf2cf40
aelmosalamy <adham@elmosalamy.net>
2019-10-19 10:07:16 +0400

    New Directory Structure where the Pelican blog isnt the main site, instead, My Pelican blog is now a sub-directory under /blog while /index.html is now the main entry page to the site, this allows me to control the website flow and add my own projects and demos for showcasing or just for testing under live server conditions.

---
+++ portfolio/app.js
@@ -0,0 +1,40 @@
+function go(id) {
+  var element = document.getElementById(id); 
+  element.scrollIntoView({behavior: "smooth"});
+}
+
+function displayProjects() {
+  projects = JSON.parse(this.responseText);
+  projectsDOM = document.getElementById("projects");
+  for (i = projects.length - 1; i >= 0; i--) {
+    p = projects[i]; // extract project object from the projects list obtained from 'projects.json'
+    // fill in the blanks using object notation
+    tags_str = ""
+    if (p.tags) {
+      for (tag of p.tags) {
+        tags_str = tags_str + `<span class="tag">${tag}</span>`
+      }
+    }
+    let cardTemplate = `
+        <div data-aos="fade-up" class="project-tile">
+          <p class="project-name">${p.name}</p>
+          <p class="project-description">${p.description}</p>
+          <div class="project-tags">
+            ${tags_str}
+          </div>
+          <img class="project-screenshot" src="${p.screenshot_url}">
+          <div class="project-links">
+            <a href="${p.demo_url}" target="_blank" class="far fa-eye btn project-demo"></a>
+            <a href="${p.source_url}" target="_blank" class="fas fa-file-code btn project-source"></a>
+          </div>
+        </div>`
+    projectsDOM.insertAdjacentHTML('beforeend', cardTemplate);
+  }
+}
+
+var oReq = new XMLHttpRequest();
+oReq.addEventListener("load", displayProjects);
+oReq.open("GET", "projects.json");
+oReq.send();
+
+// window.onload = displayProjects();
