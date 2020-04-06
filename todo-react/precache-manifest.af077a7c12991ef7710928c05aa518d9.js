8a0e09fbe45c1992bce8103aa80d3a39ca0da36f
Adham Elmosalamy <adham@elmosalamy.net>
2020-04-06 20:32:27 +0400

    Added an article

---
+++ todo-react/precache-manifest.af077a7c12991ef7710928c05aa518d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdc569ce9040df564a79485335a62493",
+    "url": "/index.html"
+  },
+  {
+    "revision": "410c7fd055b16ac65f57",
+    "url": "/static/css/main.7664f59d.chunk.css"
+  },
+  {
+    "revision": "98236e1a87a01a80b855",
+    "url": "/static/js/2.57de41d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.57de41d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "410c7fd055b16ac65f57",
+    "url": "/static/js/main.be31cfa5.chunk.js"
+  },
+  {
+    "revision": "d9726643840916ac66e3",
+    "url": "/static/js/runtime-main.afba523e.js"
+  }
+]);
