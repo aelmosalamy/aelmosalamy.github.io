b04f2dab9bb6a7af25400b9df057d2ad45cce5ae
Adham Elmosalamy <adham@elmosalamy.net>
2020-04-09 22:41:06 +0400

    Added random-quote-machine

--- random-quote-machine/app.js
+++ random-quote-machine/app.js
@@ -9,10 +9,10 @@ var quoteBox = document.getElementById("quote-box")
 var quoteBoxContent = document.getElementById("box-content")
 
 // Init
-window.onload = () => {
+window.onload = function () {
   // Creating a global variable 'request'
   window.request = new XMLHttpRequest()
-  request.addEventListener('load', () => {
+  request.addEventListener('load', function () {
   // request.response is an Object that only exists after request 'load'
   // Parsing the response string
   window.jsonQuoteData = JSON.parse(request.response)
@@ -26,30 +26,30 @@ window.onload = () => {
 }
 
 // New quote onclick handler
-newQuote.onclick = () => {
+newQuote.onclick = function () {
   // Stylishly fade the quoteBox's inner contents and reveal after DOM has been tweaked
   // Total transition time 400ms
   quoteBoxContent.classList.add("fade") // 200ms to finish
-  setTimeout(() => {
+  setTimeout(function () {
     displayNewQuote(quotes) // Display a new quote
     quoteBoxContent.classList.remove("fade") // another 200ms to finish
   }, 200) // Remove .fade after transition completion
 }
 
 // Given a list of quotes, randomly choose one then tweek the DOM to display it
-const displayNewQuote = (quotes) => {
+const displayNewQuote = function(quotes) {
   var quoteIndex = Math.floor(Math.random() * quotes.length)
   var quote = quotes[quoteIndex]
   var text, author
   [text, author] = [quote[0], quote[1]]
   setQuote(text, author)
   // Change the href of tweet-quote button
-  var newHref = `https://twitter.com/intent/tweet?hashtags=quotes,wisdom&text=${text + ' - ' + author}`
+  var newHref = "https://twitter.com/intent/tweet?hashtags=quotes,wisdom&text=" + text + ' - ' + author
   tweetQuote.href = newHref
 }
 
 // Given a quote's text and author, tweek the DOM to display each parameter
-const setQuote = (text, author) => {
+const setQuote = function(text, author) {
   quoteText.innerHTML = text
   quoteAuthor.innerHTML = author
 }
